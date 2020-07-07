cee069c0624f27fb0f3f4e0bb43347eb23174a71
anuraghazra <hazru.anurag@gmail.com>
2020-07-07 22:55:15 +0530

    feat: added better notification component

--- client/src/@convoy-ui/Toast.tsx
+++ client/src/@convoy-ui/Toast.tsx
@@ -1,18 +1,35 @@
 import React from "react";
-import styled from "styled-components/macro";
-import { notify } from "react-notify-toast";
+import styled, { css, CSSProp } from "styled-components/macro";
+import { store as notify } from "react-notifications-component";
 import {
-  FaTimes,
   FaInfoCircle,
   FaCheckCircle,
   FaExclamationCircle,
 } from "react-icons/fa";
+import "react-notifications-component/dist/theme.css";
+import theme from "styles/theme";
 
-const StyledToastText = styled.div`
+const ToastTypes: Record<string, CSSProp> = {
+  warning: css`
+    color: ${theme.colors.redDark};
+    background-color: ${theme.colors.red};
+  `,
+  success: css`
+    color: ${theme.colors.greenDark};
+    background-color: ${theme.colors.primary};
+  `,
+  info: css`
+    color: ${theme.colors.white};
+    background-color: ${theme.colors.gray};
+  `,
+};
+
+const StyledToastText = styled.div<{ type: string }>`
   display: flex;
   justify-content: center;
   align-items: center;
   .notification__icon {
+    min-width: 30px;
     margin-right: 10px;
   }
   .close-icon {
@@ -24,29 +41,87 @@ const StyledToastText = styled.div`
       transition: 0.2s;
     }
   }
+
+  margin: auto;
+  width: max-content;
+  padding: 10px 20px;
+  border-radius: 5px;
+
+  ${p => ToastTypes[p.type as any]}
 `;
 
 export const Toast: React.FC<{
   icon?: any;
-}> = ({ icon: Icon, children }) => {
+  type: string;
+}> = ({ icon: Icon, children, type }) => {
   return (
-    <StyledToastText>
+    <StyledToastText type={type}>
       {Icon && <Icon className="notification__icon" />}
       {children}
-      <FaTimes onClick={notify.hide} className="close-icon" />
     </StyledToastText>
   );
 };
 
+const defaultConfig = {
+  width: 350,
+  insert: "bottom",
+  container: "bottom-center",
+  animationIn: ["animated", "fadeIn"],
+  animationOut: ["animated", "fadeOut"],
+  dismiss: {
+    duration: 50000,
+    onScreen: false,
+    pauseOnHover: true,
+    waitForAnimation: false,
+    showIcon: true,
+    click: true,
+    touch: true,
+  },
+  slidingEnter: {
+    duration: 300,
+    timingFunction: "ease-in",
+    delay: 0,
+  },
+  slidingExit: {
+    duration: 300,
+    timingFunction: "ease-out",
+    delay: 0,
+  },
+};
+
 export const toast = {
   error: (message: string) => {
-    notify.show(<Toast icon={FaExclamationCircle}>{message}</Toast>, "error");
+    notify.addNotification({
+      type: "warning",
+      content: (
+        <Toast type="warning" icon={FaExclamationCircle}>
+          {message}
+        </Toast>
+      ),
+      ...(defaultConfig as any),
+    });
   },
   success: (message: string) => {
-    notify.show(<Toast icon={FaCheckCircle}>{message}</Toast>, "success");
+    notify.addNotification({
+      type: "success",
+      content: (
+        <Toast type="success" icon={FaCheckCircle}>
+          {message}
+        </Toast>
+      ),
+      ...(defaultConfig as any),
+    });
   },
   info: (message: string) => {
-    notify.show(<Toast icon={FaInfoCircle}>{message}</Toast>, "info");
+    notify.addNotification({
+      type: "info",
+      content: (
+        <Toast type="info" icon={FaInfoCircle}>
+          {message}
+        </Toast>
+      ),
+      ...(defaultConfig as any),
+    });
   },
 };
 
